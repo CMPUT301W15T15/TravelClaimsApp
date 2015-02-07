38f203ed3e70b2b18cb5799ba87542ee429fbb6f
Chris Wang <chriswang@uws123515usr.uws.ualberta.ca>
2015-02-07 13:06:02 -0700

    add TagListTest for JUnit test

---
+++ TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/tageListTest.java
@@ -0,0 +1,31 @@
+package com.cmput301w15t15.travelclaimsapp.test;
+
+import junit.framework.TestCase;
+
+public class tageListTest extends TestCase {
+	private TagList tagList;
+	private Tag tag1;
+	private Tag tag2;
+	
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		tagList=TagListController.getTagList();
+		tag1=new Tag("Business");
+		tag2=new Tag("Personal");
+		tagList.addTag(tag1);
+		tagList.addTag(tag2);
+		
+	}
+	public tageListTest(){
+		assertTrue("The length of the claimList is not two",  this.tagList.size()==2);
+		assertTrue("claim1 was not added", this.tagList.getTag("Business") == this.tag1);
+	}
+	public void removeTagTest(){
+		tagList.removeTag("Business");
+		assertTrue("The length of the claimList is not two",  this.tagList.size()==1);
+		assertTrue("claim1 was not added", this.tagList.getTag("Personal") == this.tag2);
+	}
+	
+}
