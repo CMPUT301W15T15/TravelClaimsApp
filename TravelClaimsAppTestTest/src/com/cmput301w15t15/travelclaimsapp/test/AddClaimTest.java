101bca49dbd8d00cb44203896b78932fcae0ae20
Bo-z <bzhou2@ualerta.ca>
2015-02-10 08:25:18 -0700

    edit addClaimTest

--- TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/AddClaimTest.java
+++ TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/AddClaimTest.java
@@ -5,11 +5,11 @@ import android.test.ActivityInstrumentationTestCase2;
 import junit.framework.TestCase;
 
 
-<<<<<<< HEAD:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/addClaimTest.java
+// <<<<<<< HEAD:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/addClaimTest.java
 public class addClaimTest extends TestCase {
-=======
+//=======
 public class claimTest extends TestCase{
->>>>>>> b754c82f7b1d5e226a1058c09d770cc99edb7d0b:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/AddClaimTest.java
+// >>>>>>> b754c82f7b1d5e226a1058c09d770cc99edb7d0b:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/AddClaimTest.java
 	private ClaimList claimList;
 	private Claim claim1;
 	private Claim claim2;
@@ -24,14 +24,14 @@ public class claimTest extends TestCase{
 		claim2 = new Claim("Claim2");
 		claimList.addClaim(claim2);
 	}
-<<<<<<< HEAD:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/addClaimTest.java
+// <<<<<<< HEAD:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/addClaimTest.java
 	public addClaimTest(){
 		assertTrue("The length of the claimList is not two",  this.claimList.size()==2);
 =======
 	public void addClaimTest(){
 		claimList.addClaim(new Claim("Claim3"));
 		assertTrue("The length of the claimList is not two", this.claimList.size() == 3);
->>>>>>> b754c82f7b1d5e226a1058c09d770cc99edb7d0b:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/AddClaimTest.java
+// >>>>>>> b754c82f7b1d5e226a1058c09d770cc99edb7d0b:TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/AddClaimTest.java
 		assertTrue("claim1 was not added", this.claimList.getClaim("Claim1") == this.claim1);
 	}
 	
