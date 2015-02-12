dc53e264dc460e41f7d9e4c1bc3cde5ee15e085b
Bo-z <bzhou2@ualberta.ca>
2015-02-12 16:44:12 -0700

    init approve test

---
+++ TravelClaimsAppTestTest/src/ApproveTest/ClaimSubmitTest.java
@@ -0,0 +1,27 @@
+package ApproveTest;
+
+import junit.framework.TestCase;
+
+public class ClaimSubmitTest extends TestCase {
+
+	public ClaimSubmitTest() {
+		super();
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testSubmitButton{
+		
+	}
+	
+	public void testSubmitedEditable{
+		
+	}
+	
+	public void testSubmitViewDisplay{
+		
+	}
+
+}
---
+++ TravelClaimsAppTestTest/src/com/cmput301w15t15/travelclaimsapp/test/ClaimStatusTest.java
@@ -0,0 +1,15 @@
+package com.cmput301w15t15.travelclaimsapp.test;
+
+import junit.framework.TestCase;
+
+public class ClaimStatusTest extends TestCase {
+
+	public ClaimStatusTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
