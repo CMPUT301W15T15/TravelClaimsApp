7e4f68604a1c9aa51c6a9c9b3a9c38b89ab23b8b
chriswang2468 <xinchao@ualberta.ca>
2015-02-12 20:09:26 -0700

    add flagExpenseTest(), and create ExpenseListViewActivity and context menu

--- TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/EditExpenseActivity.java
+++ TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/EditExpenseActivity.java
@@ -22,5 +22,36 @@ public class EditExpenseActivity extends Activity {
 		getMenuInflater().inflate(R.menu.edit_expense, menu);
 		return true;
 	}
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		switch(item.getItemId()) {
+		case R.id./*
+		 * This is an Application template. Change the name
+		 * of the class (class_name) appropriately, and put
+		 * your code in the main method.
+		 */
 
+		// Replace class_name with the first
+		// part of the file name.  For example:
+		//    class_name: MyClass
+		//    file name : MyClass.java
+		// Here's an incorrect example:
+		//    class_name: MyClass
+		//    file name : MyFile.java
+		public class class_name
+		{
+
+			// This is the main method.
+			public static void main(String[] arguments)
+			{
+
+				// Put your code in this method.
+				// Here's a sample statement.
+				System.out.println("Hi.");
+				
+
+			}
+		}:
+		adapter.remove(adapter.getItem(info.position));
+		return true;
 }
