0416953f69e0bbb64cabac368310b2d78c896768
Bo-z <bzhou2@ualberta.ca>
2015-03-08 18:12:46 -0600

    modify userList(add ArrayList)

--- TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/model/UserList.java
+++ TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/model/UserList.java
@@ -1,10 +1,17 @@
 package com.cmput301w15t15.travelclaimsapp.model;
 
+import java.util.ArrayList;
+
 public class UserList {
+	private ArrayList<User> userList;
 
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return userList.size();
+	}
+	
+	public ArrayList<User> getUserList(){
+		return this.userList;
 	}
 	
 }
