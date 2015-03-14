802804b6df8530c00286b840e5ad5364c8fb0245
machinsk <machinsk@gmail.com>
2015-03-13 22:11:31 -0600

    User login almost working

--- TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/model/User.java
+++ TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/model/User.java
@@ -1,18 +1,21 @@
 package com.cmput301w15t15.travelclaimsapp.model;
 
+import java.util.ArrayList;
+
 /**
  * @author machinsk
  *
  * User Class: intended to store usernames, hashed passwords, and approver boolean for a given user. 
  *
  */
-public class User {
+public class User  implements Listenable{
 
 	private String username;
 	
 	//private ClaimList claimList;
 	private byte[] pHash;
 	private boolean approver;
+	protected transient ArrayList<Listener> listeners;
 	
 	
 	/**
@@ -73,6 +76,27 @@ public class User {
 	 */
 	public boolean isApprover() {
 		return approver;
+	}
+
+
+	public void notifyListeners() {
+		for (Listener listener : listeners) {
+			listener.update();
+		}
+	}
+	
+	public void addListener(Listener listener) {
+		listeners.add(listener);
+	}
+	
+	public void deleteListener(Listener listener){
+		listeners.remove(listener);
+	}
+
+	@Override
+	public void setListeners() {
+		this.listeners = new ArrayList<Listener>();
+		
 	}
 
 	
