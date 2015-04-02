ad83c26478f4113085bfc4bcea752d977590ce3f
machinsk <machinsk@gmail.com>
2015-04-02 17:28:29 -0600

    Fixed FileManager, and Submit claim added but not implemented

--- TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/activitys/MainMenuActivity.java
+++ TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/activitys/MainMenuActivity.java
@@ -205,9 +205,7 @@ public class MainMenuActivity extends Activity {
 					if(Arrays.equals(pulledUser.getpHash(), givenPassHash)){
 						FileManager.getSaver().saveUserInFile(pulledUser);
 						ClaimList claimlist = FileManager.getSaver().getClaimList(givenUsername);
-						ClaimList approveClaimList = FileManager.getSaver().getApproveClaimList(givenUsername);
 						FileManager.getSaver().saveClaimLInFile(claimlist, givenUsername);
-						FileManager.getSaver().saveApproveClaimLInFile(approveClaimList, givenUsername);
 						runOnUiThread(launchAddClaims);
 					} else {
 						runOnUiThread(popToast);
