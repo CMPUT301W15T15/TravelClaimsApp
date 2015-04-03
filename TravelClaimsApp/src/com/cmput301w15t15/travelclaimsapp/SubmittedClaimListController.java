55b77e1a361c9ac685b57ea1cdca1f96f62164d9
scheidemanS <searn@ualberta.ca>
2015-04-03 14:12:07 -0600

    refactoring submitted claimlist controller

--- TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/SubmittedClaimListController.java
+++ TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/SubmittedClaimListController.java
@@ -6,10 +6,6 @@ import java.util.Locale;
 
 import com.cmput301w15t15.travelclaimsapp.model.Claim;
 import com.cmput301w15t15.travelclaimsapp.model.ClaimList;
-import com.cmput301w15t15.travelclaimsapp.model.ClaimListSaveListener;
-import com.cmput301w15t15.travelclaimsapp.model.Destination;
-import com.cmput301w15t15.travelclaimsapp.model.DestinationList;
-import com.cmput301w15t15.travelclaimsapp.model.Expense;
 import com.cmput301w15t15.travelclaimsapp.model.ExpenseList;
 import com.cmput301w15t15.travelclaimsapp.model.Listener;
 import com.cmput301w15t15.travelclaimsapp.model.Tag;
@@ -33,7 +29,6 @@ public class SubmittedClaimListController
 				@Override
 				public void update() {
 					save();
-					submittedClaimList.sort();
 					
 				}
 			});
@@ -44,7 +39,6 @@ public class SubmittedClaimListController
 			}
 		}
 	}
-	
 	
 	/**
 	 * Returns the global application claimList
@@ -63,7 +57,6 @@ public class SubmittedClaimListController
 				@Override
 				public void update() {
 					save();
-					submittedClaimList.sort();
 					
 				}
 			});
@@ -85,8 +78,6 @@ public class SubmittedClaimListController
 	 * 
 	 * @return all unique tags in the singleton claimlist
 	 */
-	
-	
 	public static ArrayList<Tag> getTagList(){
 		TagList tags = new TagList();
 		
@@ -100,9 +91,6 @@ public class SubmittedClaimListController
 		}
 		return tags.toArrayList();
 	}
-	
-	
-	
 	
 	/**
 	 * Uses ClaimList.addClaim to add claim with listeners to claimList. 
@@ -125,65 +113,6 @@ public class SubmittedClaimListController
 	}
 	
 	
-	/**
-	 * Uses Claim.addExpense() to add a expense with listener to claim
-	 * 
-	 * @param expense	the Expense to be added to claim
-	 * @param claim		the Claim to add expense to
-	 */
-	public static void addExpense(Expense expense, Claim claim){
-		claim.addExpense(expense);
-		claim.getExpenseList().sort();
-		expense.addListener(new ClaimListSaveListener());
-	}
-	
-	/**
-	 * Removes expense from singleton Claimlist
-	 */
-	public static void removeExpense(Expense expense, Claim claim){
-		claim.removeExpense(expense);
-	}
-	
-	/**
-	 * Adds a destination to a claim
-	 * 
-	 * @param dest 	destination to add to claim
-	 * @param claim claim to add destination to 
-	 */
-	public static void addDestination(Destination dest, Claim claim){
-		claim.getDestinationList().addDestination(dest);
-		dest.addListener(new ClaimListSaveListener());
-	}
-	/**
-	 * Deletes a destination from a claim
-	 * 
-	 * @param dest	destination to delete
-	 * @param claim removes destination from this claim
-	 */
-	public static void removeDestination(Destination dest, Claim claim){
-		claim.getDestinationList().deleteDestination(dest);
-		
-	}
-	/**
-	 * Adds a tag to a given claim
-	 * 
-	 * @param claim claim to add tag to 
-	 * @param tag   tag to add to claim
-	 */
-	public static void addTag(Claim claim, Tag tag) {
-		tag.addListener(new ClaimListSaveListener());
-		claim.getTagList().addTag(tag);
-	}
-	
-	/**
-	 * Removes a {@link Tag} from a given {@link Claim}
-	 * 
-	 * @param claim claim to delete tag from 
-	 * @param tag   tag to delete
-	 */
-	public static void removeTag(Claim claim, Tag tag) {
-		claim.getTagList().removeTag(tag);
-	}
 	
 	/**
 	 * Saves claimlist to file using FileManager class 
@@ -210,44 +139,9 @@ public class SubmittedClaimListController
 		claim.addListener(new Listener() {
 			@Override
 			public void update() {
-				getClaimList().sort();
 				save();
 			}
 		});
-		//next add listener to expenselist
-		ExpenseList eList = claim.getExpenseList();
-		eList.setListeners();
-		eList.addListener(new ClaimListSaveListener());
-		//next add a listener to each expense in the claim 
-		for(Expense expense : eList.toArrayList()){
-			expense.setListeners();
-			expense.addListener(new ClaimListSaveListener());
-			if(expense.getGeoLocation() != null){
-				expense.getGeoLocation().setListeners();
-				expense.getGeoLocation().addListener(new ClaimListSaveListener());
-			}
-		}
-		
-		DestinationList dList = claim.getDestinationList();
-		dList.setListeners();
-		dList.addListener(new ClaimListSaveListener());
-		for(Destination dest : dList.toArrayList()){
-			dest.setListeners();
-			dest.addListener(new ClaimListSaveListener());
-			if(dest.getGeoLocation() != null){
-				dest.getGeoLocation().setListeners();
-				dest.getGeoLocation().addListener(new ClaimListSaveListener());
-			}
-		}
-		TagList tList = claim.getTagList();
-		tList.setListeners();
-		tList.addListener(new ClaimListSaveListener());
-		for(Tag tag : tList.toArrayList()){
-			tag.setListeners();
-			tag.addListener(new ClaimListSaveListener());
-			
-		}
-		
 	}
 
 
