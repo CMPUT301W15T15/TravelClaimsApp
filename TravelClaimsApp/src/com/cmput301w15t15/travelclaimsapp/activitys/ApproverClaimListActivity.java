f17e9ca522b13e5804551a9fd6a9b35696cb9357
scheidemanS <searn@ualberta.ca>
2015-04-03 21:23:05 -0600

    approver stuff

--- TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/activitys/ApproverClaimListActivity.java
+++ TravelClaimsApp/src/com/cmput301w15t15/travelclaimsapp/activitys/ApproverClaimListActivity.java
@@ -147,15 +147,24 @@ public class ApproverClaimListActivity extends Activity {
             	alertd.show();
             	return true;
             case R.id.cmenu_approve:
-            	claim.setStatus(Claim.APPROVED);
-            	Thread approvedThread = new saveChangesThread(claimList, claim, this);
-            	approvedThread.start();
+            	if(canApprove(claim)){
+            		claim.setStatus(Claim.APPROVED);
+            		claim.addComment(claim.getComment());
+            		claimList.removeClaim(claimList.getClaim(claim.getName()));
+            		claimAdaptor.notifyDataSetChanged();
+            		Thread approvedThread = new saveChangesThread(claimList, claim, this);
+            		approvedThread.start();
+            	}
             
             case R.id.cmenu_return:
-            	claim.setStatus(Claim.RETURNED);
-            	Thread returnedThread = new saveChangesThread(claimList, claim, this);
-            	returnedThread.start();
-            	
+            	if(canApprove(claim)){
+            		claim.setStatus(Claim.RETURNED);
+            		claim.addComment(claim.getComment());
+            		claimList.removeClaim(claimList.getClaim(claim.getName()));
+            		claimAdaptor.notifyDataSetChanged();
+            		Thread returnedThread = new saveChangesThread(claimList, claim, this);
+            		returnedThread.start();
+            	}
             default:
                 return super.onContextItemSelected(item);
         }
@@ -206,7 +215,6 @@ public class ApproverClaimListActivity extends Activity {
 				claimantClaimList.removeClaim(claimantClaimList.getClaim(modifiedClaim.getName()));
 				claimantClaimList.addClaim(modifiedClaim);
 				FileManager.getSaver().addClaimList(claimantClaimList, modUsername);
-				
 				FileManager.getSaver().addSubmittedClaimL(newSubmittedClaimList);
 			}
 		
